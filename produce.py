879b72fbe1d142136b95ccaba603111ccfd68d77
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2022-04-07 15:53:02 +0200

    Successfully implement frequency sliding and phase smoothing

--- produce.py
+++ produce.py
@@ -4,6 +4,7 @@ from pydub import AudioSegment
 from pydub.effects import normalize
 
 import math
+from mpmath import sinpi as slow_sinpi
 import struct
 import time
 
@@ -13,9 +14,47 @@ assert SAMPLE_WIDTH == 4
 SAMPLE_RATE = 48000
 SHEPHERD_PERIOD_SAMPLES = SAMPLE_RATE * 5  # 5 seconds
 
+SHEPERD_BASE_FREQUENCY = 330  # Goes up an octave
+
+
+def sinpi(x):
+    # Slow:
+    # return slow_sinpi(x)
+    # Fast but slightly inaccurate:
+    return math.sin(math.pi * x)
+
+
+def compute_integer_place_near_1(coefficient):
+    # Assume f(x) = coefficient * (2 ** x - 1)
+    # We want to find an x close to 1 such that f(x) is integer.
+    y_up = math.ceil(coefficient)
+    x_up = math.log2(y_up / coefficient + 1)
+    y_down = math.floor(coefficient)
+    x_down = math.log2(y_down / coefficient + 1)
+    assert 0.9 < x_down <= 1 <= x_up < 1.1, (coefficient, x_down, x_up)
+    # Because x will end up multiplicative, we can't just compare (x_up - 1) and (1 - x_down)
+    error_up = x_up / 1
+    error_down = 1 / x_down
+    #print(f'y={y_down}/{y_up}, x={x_down}/{x_up}, error={error_down}/{error_up}')
+    if error_up < error_down:
+        return x_up
+    else:
+        return x_down
+
 
 def make_waveform():
-    return [round(1e3 * math.sin(t * 2 * math.pi * 440 / SAMPLE_RATE)) for t in range(SHEPHERD_PERIOD_SAMPLES)]  # FIXME
+    additional_samples = 0.0  # FIXME, can be negative, can be fractional
+    waveform = []
+    base_coefficient = SHEPERD_BASE_FREQUENCY * SHEPHERD_PERIOD_SAMPLES / (SAMPLE_RATE * math.log(2))
+    extra_time = compute_integer_place_near_1(base_coefficient) - 1
+    for t in range(SHEPHERD_PERIOD_SAMPLES):  # Integer in [0, SHEPHERD_PERIOD_SAMPLES). Unit: samples
+        t_0_1 = t / SHEPHERD_PERIOD_SAMPLES  # Real in [0, 1). Unit: fraction of whole waveform
+        t_real_0_1 = t_0_1 + (-2 * t_0_1 + 3) * t_0_1 * t_0_1 * extra_time  # Real in [0, 1±epsilon). Unit: fraction of whole waveform
+        phase = base_coefficient * 2 * (2 ** t_real_0_1 - 1)
+        waveform.append(round(1e6 * sinpi(phase)))
+    print(f'Final phase: {phase}')
+
+    return waveform
 
 
 def save_waveform(waveform):
